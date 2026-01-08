cf4b614534863157c5c2a1e23f60fa91797d4652
inwokolo001 <i.nwokolo001@umb.edu>
2026-01-08 14:47:47 -0500

    I created the script.js for the password generator
    I added starter JavaScript for password generator.

---
+++ web-ui/script.js
@@ -0,0 +1,12 @@
+// Starter JavaScript for password generator
+// Start here
+
+function generatePassword() {
+  // Decide how long the password should be
+
+  // Decide what characters to include
+
+  // Generate the password
+
+  // Show it on the page
+}
