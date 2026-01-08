ff6c007b83617e448b557bd106e41acf5ae90ea0
inwokolo001 <i.nwokolo001@umb.edu>
2026-01-08 14:45:19 -0500

    making the Main.java template

---
+++ java-script/Main.java
@@ -0,0 +1,22 @@
+// Starter Java password generator
+// Start here
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Password generator starting point");
+
+        // Decide how long the password should be
+
+        // Decide what types of characters to include
+        // (letters, numbers, symbols)
+
+        // Generate the password
+
+        // Show the result to the user
+
+        // If you want something a bit more complex, you can try to:
+        // - check how strong the password is
+        // - use SecureRandom instead of Random
+        // - refactor the logic into helper methods
+    }
+}
