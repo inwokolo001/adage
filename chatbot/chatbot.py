af8cf6e394906f7b9b8ab8aca9afc53d364190a1
inwokolo001 <i.nwokolo001@umb.edu>
2026-01-08 14:50:32 -0500

    I added the starter code for password security chatbot

---
+++ chatbot/chatbot.py
@@ -0,0 +1,9 @@
+# Starter password security chatbot
+# Start here
+
+print("Password security chatbot starting point")
+
+# Think about:
+# - common password questions
+# - simple explanations
+# - how users might interact with this
